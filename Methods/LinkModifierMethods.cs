bcf3955c90860c4afbe18bcb12034216708b31d4
Shoey <shoeyfiller@gmail.com>
2024-08-15 22:10:30 -0400

    link modifier exception tweak

--- Methods/LinkModifierMethods.cs
+++ Methods/LinkModifierMethods.cs
@@ -49,17 +49,20 @@ namespace Instance_Manager.Methods
                     if (MessageBox.Show("Could not find directory " + ReplaceVariables(TextInputString) + ". Create directory?", "Warning", MessageBoxButtons.YesNo) == DialogResult.Yes)
                     {
                         try { 
-                        Directory.CreateDirectory(ReplaceVariables(TextInputString));
+                         Directory.CreateDirectory(ReplaceVariables(TextInputString));
+                        }
+                        catch (Exception ex)
+                        {
+                            ThreadedMessage("Couldn't create directory\n" + ReplaceVariables(TextInputString));
+                            WriteLineIfDebug(ex.Message);
+                            return;
+                        }
                         if (second)
                             ProfileDirectoryLinks[ProfileDirectoryLinks.IndexOf(link)] = splitlink[0] + "|" + TextInputString;
                         else
                             ProfileDirectoryLinks[ProfileDirectoryLinks.IndexOf(link)] = TextInputString + "|" + splitlink[1];
                         SaveProfileLinks();
                         NeedLinkMenuRefresh = true;
-                        }
-                        catch {
-                            ThreadedMessage("Couldn't create directory\n"+ ReplaceVariables(TextInputString));
-                        }
                     }
                 }
             }
